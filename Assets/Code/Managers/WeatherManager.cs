26faab7e3a923293c0c6be50d7e52d3853dda364
Doomzer <sharapov-tolik-1@mail.ru>
2018-04-01 19:39:42 +0300

    Changing sky box

---
+++ Assets/Code/Managers/WeatherManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Xml;
+
+public class WeatherManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+    public float cloudValue { get; private set; } //Облачность редактируется внутренне, в остальных местах это свойство предназначено только для чтения.
+    private NetworkService _network;
+
+    public void OnXMLDataLoaded(string data)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(data); // Разбиваем XML-код на структуру с возможностью поиска.
+        XmlNode root = doc.DocumentElement;
+        XmlNode node = root.SelectSingleNode("clouds"); // Извлекаем из данных один узел.
+        string value = node.Attributes["value"].Value;
+        cloudValue = Convert.ToInt32(value) / 100f; // Преобразуем значение в число типа float в диапазоне от 0 до 1.
+        Debug.Log("Value: " + cloudValue);
+        //Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
+        status = ManagerStatus.Started;
+    }
+
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Weather manager starting...");
+        _network = service; //Сохранение вставленного объекта NetworkService.
+        status = ManagerStatus.Started;
+    }
+}
---
+++ Assets/Code/WeatherController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky; //Ссылаться можно не только на объекты сцены, но и на материал на вкладке Project.
+    [SerializeField] private Light sun;
+    private float _fullIntensity;
+    private float _cloudValue = 0f;
+    private bool loop = false;
+
+    void Start()
+    {
+        _fullIntensity = sun.intensity; //Исходная интенсивность света считается «полной».
+    }
+
+    void Update()
+    {
+        SetOvercast(_cloudValue);
+
+        if (loop == false)
+          _cloudValue += .005f; // Для непрерывности перехода увеличивайте значение в каждом кадре.
+        else
+            _cloudValue -= .005f;
+
+        if (_cloudValue > 1)
+            loop = true;
+
+        if (_cloudValue < 0)
+            loop = false;
+    }
+
+    private void SetOvercast(float value) //Корректируем как значение Blend материала, так и интенсивность света.
+    {
+        sky.SetFloat("_Blend", value);
+        sun.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+}
